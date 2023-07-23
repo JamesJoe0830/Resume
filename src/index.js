2aec70b942dfd2495be7dbff20fec3f3222022f2
james.joe <kyeun950830@gmail.com>
2023-07-23 19:10:30 +0900

    gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter basename='Resume'>
+        <BrowserRouter basename='/resume'>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
